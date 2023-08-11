24b9e8c852b8b8a3bbfd7ad41fd2f7fad5faf42b
syun031 <140134707+syun031@users.noreply.github.com>
2023-08-11 18:10:46 +0900

    first commit（追加修正）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occuoation, :position])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
   end
 end
