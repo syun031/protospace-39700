ea084c77268278ab5852b9752113c6e65776eea6
syun031 <140134707+syun031@users.noreply.github.com>
2023-08-14 12:18:52 +0900

    訂正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -47,7 +47,7 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image, :name).merge(user_id: current_user.id)
   end
 
   def set_prototype
